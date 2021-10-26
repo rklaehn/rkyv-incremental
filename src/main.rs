9aa14b6fc18c2ff4c65b8f6eed59639fc369cd1b
Ruediger Klaehn <rklaehn@protonmail.com>
2021-10-26 13:08:28 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,91 @@ use std::sync::Arc;
 
 use rkyv::{
     archived_root, check_archived_root,
-    de::deserializers::SharedDeserializeMap,
+    de::{deserializers::SharedDeserializeMap, SharedDeserializeRegistry},
     ser::{
         serializers::{AlignedSerializer, AllocSerializer, CompositeSerializer},
-        Serializer,
+        ScratchSpace, Serializer, SharedSerializeRegistry,
     },
-    Deserialize,
+    Archive, Archived, Deserialize, Resolver, Serialize,
 };
+struct Tree {
+    prefix: String,
+    value: Option<String>,
+    children: Arc<Vec<Tree>>,
+}
+
+struct ArchivedTree {
+    prefix: Archived<String>,
+    value: Archived<Option<String>>,
+    children: Archived<Arc<Vec<Tree>>>,
+}
+
+struct TreeResolver {
+    prefix: Resolver<String>,
+    value: Resolver<Option<String>>,
+    children: Resolver<Arc<Vec<Tree>>>,
+}
+
+fn offset_from<T, U>(base: *const T, p: *const U) -> usize {
+    let base = base as usize;
+    let p = p as usize;
+    assert!(p >= base);
+    p - base
+}
+
+impl Archive for Tree {
+    type Archived = ArchivedTree;
+
+    type Resolver = TreeResolver;
+
+    unsafe fn resolve(&self, pos: usize, resolver: Self::Resolver, out: *mut Self::Archived) {
+        let TreeResolver {
+            prefix,
+            value,
+            children,
+        } = resolver;
+        let ptr = &mut (*out).prefix;
+        self.prefix
+            .resolve(pos + offset_from(out, ptr), prefix, ptr);
+        let ptr = &mut (*out).value;
+        self.value.resolve(pos + offset_from(out, ptr), value, ptr);
+        let ptr = &mut (*out).children;
+        self.children
+            .resolve(pos + offset_from(out, ptr), children, ptr);
+    }
+}
+
+impl<S> Serialize<S> for Tree
+where
+    S: Serializer + SharedSerializeRegistry + ScratchSpace,
+{
+    fn serialize(&self, serializer: &mut S) -> Result<Self::Resolver, S::Error> {
+        let prefix = self.prefix.serialize(serializer)?;
+        let value = self.value.serialize(serializer)?;
+        let children = self.children.serialize(serializer)?;
+        Ok(TreeResolver {
+            prefix,
+            value,
+            children,
+        })
+    }
+}
+
+impl<D> Deserialize<Tree, D> for ArchivedTree
+where
+    D: SharedDeserializeRegistry + ?Sized,
+{
+    fn deserialize(&self, deserializer: &mut D) -> std::result::Result<Tree, D::Error> {
+        let prefix: String = self.prefix.deserialize(deserializer)?;
+        let value: Option<String> = self.value.deserialize(deserializer)?;
+        let children: Arc<Vec<Tree>> = self.children.deserialize(deserializer)?;
+        Ok(Tree {
+            prefix,
+            value,
+            children,
+        })
+    }
+}
 
 fn main() {
     let x = Arc::new("foo".to_owned());
